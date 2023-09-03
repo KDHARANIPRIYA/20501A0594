8a0fda97f071789c7b0f57c8ed8e8117ba73bdf3
KDHARANIPRIYA <103330033+KDHARANIPRIYA@users.noreply.github.com>
2023-09-03 12:00:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+   import './App.css';
+   import NumberFetcher from './api';
+
+   function App() {
+     return (
+       <div className="App">
+         <NumberFetcher />
+       </div>
+     );
+   }
+
+   export default App;
